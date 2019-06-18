f8110e49f28de205eb36a8daeb255e3c92455f4b
lalmeida <larry.almeida@zalando.de>
2019-06-18 16:24:32 +0200

    Added table styling

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -3,19 +3,22 @@ import { Link } from "react-router-dom";
 
 import "./ProductList.css";
 
-const matchCalculator = (metrics) => metrics.reduce((x, y) => x + y, 0) / metrics.length;
-
 const Match = ({ farm_test, facility_1_test, facility_2_test, facility_3_test}) => {
-    return null;
-}
+    const metrics = [farm_test, facility_1_test, facility_2_test, facility_3_test];
+    const matchPercent = (metrics.map(metric => metric === 'NA' ? 0 : parseInt(metric, 10)).reduce((x, y) => x + y, 0) / metrics.length) * 100;
+    const styleClass = matchPercent < 40 ? 'bad-match' : matchPercent < 75 ? 'ok-match' : 'good-match';
+    return (
+        <div className={`match ${styleClass}`}>{matchPercent}% Match</div>
+    );
+};
 
-const ProductItem = ({ article, url, qty, image }) => (
+const ProductItem = ({ article, url, qty, image, ...product }) => (
   <tr>
     <td>
       <img src={image} alt={article} />
       <Link to={url}>{article}</Link>
     </td>
-    <td>OK</td>
+    <td><Match {...product} /></td>
     <td>{qty}</td>
   </tr>
 );
