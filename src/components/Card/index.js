c94ba354e5b98963e8e11d54ddaee0d0f394773d
Takahashi, NatalieMie <NatalieMie.Takahashi@adidas.com>
2019-06-18 12:20:01 +0200

    cleaning main page and adding components folder

---
+++ src/components/Card/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+const Card = ({ title}) => {
+  return (
+    <div className="Card">
+      {title}
+    </div>
+  );
+}
+
+export default Card;
