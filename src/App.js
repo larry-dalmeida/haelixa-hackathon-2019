62e1e427848e5e03d940c017ea4b80aefe6b5df2
lalmeida <larry.almeida@zalando.de>
2019-06-18 14:42:57 +0200

    Added Routing setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import './App.css';
+
+import Products from './components/Products';
+import Product from './components/Product';
+
+const products = [
+  { id: 1, name: 'Cool product 1', url: '/products/1' },
+  { id: 2, name: 'Cool product 2', url: '/products/2' },
+  { id: 3, name: 'Cool product 3', url: '/products/3' },
+  { id: 4, name: 'Cool product 4', url: '/products/4' },
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Haelixa Analytics
       </header>
+      <Router>
+          <Switch>
+            <Route exact path="/products/:id" render={routerProps => <Product {...routerProps} products={products} />} />
+            <Route exact path="/products" component={routerProps => <Products {...routerProps} products={products} />} />
+            <Route path="/" render={Products} />
+          </Switch>
+      </Router>
     </div>
   );
 }
